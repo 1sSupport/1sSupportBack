3a9f842062a1ee154c74b794f284e7554eba09a6
Mikhail <raeinforce@gmail.com>
2018-10-12 22:30:50 +0300

    Fix and add new Tagirator

---
+++ Source/WebApi.Tagirator/TagirationArticle.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebApi.EF.Models;
+using WebApi.Tagirator.Parser;
+
+namespace WebApi.Tagirator
+{
+    public class TagirationArticle
+    {
+        private readonly Article _article;
+
+        public Article Article => _article;
+
+        public TagirationArticle(Article article)
+        {
+            _article = article;
+        }
+
+        private IEnumerable<string> _cleanWords;
+
+        private IEnumerable<string> AllWords
+        {
+            get
+            {
+                if (_cleanWords != null)
+                {
+                    return _cleanWords;
+                }
+                else
+                {
+                    _cleanWords = GetCleanWords();
+                    return _cleanWords;
+                }
+            }
+        }
+
+        private IEnumerable<string> _hasSetWord;
+
+        public IEnumerable<string> CleanWords => _hasSetWord ?? (_hasSetWord = new HashSet<string>(AllWords));
+
+        private IEnumerable<string> GetCleanWords()
+        {
+            var words = FilteredText.GetWords(_article.Text);
+            return words;
+        }
+
+        private IDictionary<string, WordInfo> _wordsInfo = new Dictionary<string, WordInfo>();
+
+        private void SetFrequancy()
+        {
+            foreach (var cleanWord in AllWords)
+            {
+                if (_wordsInfo.ContainsKey(cleanWord))
+                {
+                    _wordsInfo[cleanWord].Freq++;
+                }
+                else
+                {
+                    var wordInfo = new WordInfo { Freq = 1 };
+
+                    _wordsInfo.Add(cleanWord, wordInfo);
+                }
+            }
+        }
+
+        public void SetWordRate(string word, double globalRate)
+        {
+            if (!_wordsInfo.Any())
+            {
+                SetFrequancy();
+            }
+
+            var wordInfo = _wordsInfo[word];
+            wordInfo.Rate = wordInfo.Freq - globalRate;
+        }
+
+        private void SortByRate()
+        {
+            //var word = _wordsInfo.OrderByDescending(p => p.Value.Rate);
+            _wordsInfo = _wordsInfo.OrderByDescending(p => p.Value.Rate).ToDictionary(p => p.Key, p => p.Value);
+        }
+
+        public IDictionary<string, WordInfo> GetTagsInArticle()
+        {
+            SortByRate();
+
+            return _wordsInfo;
+        }
+
+        public int GetWordFrequancy(string word)
+        {
+            if (!_wordsInfo.Any())
+            {
+                SetFrequancy();
+            }
+
+            return !_wordsInfo.ContainsKey(word) ? 0 : _wordsInfo[word].Freq;
+        }
+    }
+}
---
+++ Source/WebApi.Tagirator/Tagirator.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApi.EF.Models;
+
+namespace WebApi.Tagirator
+{
+    public static class Tagirator
+    {
+        private static readonly ICollection<TagirationArticle> TagirationArticles = new List<TagirationArticle>();
+
+        private static EFContext Context;
+
+        public static void AddContextForTagging(EFContext context)
+        {
+            Context = context ?? throw new NullReferenceException();
+        }
+
+        private static bool IsArticlesNullOrEmpty(IEnumerable<Article> articles)
+        {
+            return articles == null || !articles.Any();
+        }
+
+        private static void AddArticlesFromContext()
+        {
+            if (IsArticlesNullOrEmpty(Context.Articles))
+            {
+                throw new ArgumentException("Необходимо добавить стать в БД");
+            }
+            /*add range*/
+            var tagirationArticles = Context.Articles.Select(article => new TagirationArticle(article)).ToList();
+            foreach (var tagirationArticle in tagirationArticles)
+            {
+                TagirationArticles.Add(tagirationArticle);
+            }
+        }
+
+        private static readonly IDictionary<string, WordInfo> GlobalWordsObject = new Dictionary<string, WordInfo>();
+
+        private static void SetGlobalWords()
+        {
+            foreach (var article in TagirationArticles)
+            {
+                foreach (var word in article.CleanWords)
+                {
+                    if (GlobalWordsObject.ContainsKey(word))
+                    {
+                        var value = article.GetWordFrequancy(word);
+                        GlobalWordsObject[word].Freq += value;
+                    }
+                    else
+                    {
+                        var wordObject = new WordInfo { Freq = article.GetWordFrequancy(word) };
+
+                        GlobalWordsObject.Add(word, wordObject);
+                    }
+                }
+            }
+        }
+
+        private static double GetRatioFreq(string key)
+        {
+            if (!GlobalWordsObject.ContainsKey(key))
+            {
+                return 0;
+            }
+
+            // ReSharper disable once PossibleLossOfFraction
+            double v = GlobalWordsObject[key].Freq / TagirationArticles.Count;
+            return v;
+        }
+
+        private static void SetGlobalWordsRate()
+        {
+            if (!GlobalWordsObject.Any())
+            {
+                SetGlobalWords();
+            }
+
+            foreach (var key in GlobalWordsObject.Keys)
+            {
+                var wordObject = GlobalWordsObject[key];
+                wordObject.Rate = GetRatioFreq(key);
+            }
+        }
+
+        private static void SetLocalRate()
+        {
+            foreach (var article in TagirationArticles)
+            {
+                foreach (var word in article.CleanWords)
+                {
+                    article.SetWordRate(word, GlobalWordsObject[word].Rate);
+                }
+            }
+        }
+
+        private static void SetTagInArticle(Article article, Tag tag)
+        {
+            article.ArticleTag.Add(new ArticleTag(article, tag));
+        }
+
+        public static void SetTagsInArticle()
+        {
+            AddArticlesFromContext();
+
+            SetGlobalWords();
+            SetGlobalWordsRate();
+            SetLocalRate();
+
+            SetCurrentTag();
+        }
+
+        private static void SetCurrentTag()
+        {
+            foreach (var tagirationArticle in TagirationArticles)
+            {
+                var tagsAndRate = tagirationArticle.GetTagsInArticle().Take(10);
+
+                foreach (var pair in tagsAndRate)
+                {
+                    var dbTag = (from tag in Context.Tags where tag.Value == pair.Key select tag).FirstOrDefault();
+                    SetTagInArticle(tagirationArticle.Article, dbTag ?? new Tag(pair.Key));
+                }
+            }
+        }
+    }
+
+    public class WordInfo
+    {
+        public int Freq { get; set; }
+
+        public double Rate { get; set; }
+    }
+}
