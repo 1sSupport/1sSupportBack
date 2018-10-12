4d114c00a2009f5c44bf745c8fb6be2476297aac
Mikhail <raeinforce@gmail.com>
2018-10-12 22:31:55 +0300

    Add test for Tegirator and EFContext

--- Test/WebApi.Test/EFModelTest.cs
+++ Test/WebApi.Test/EFModelTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using WebApi.EF.Models;
 using Xunit;
 
@@ -6,29 +7,31 @@ namespace WebApi.Test
 {
     public class EFModelTest : IDisposable
     {
-        private readonly string articleText;
-        private readonly string articleTitle;
-        private readonly string tagText;
-        private readonly string configName;
-        private readonly string providerName;
-        private readonly string userLogin;
-        private readonly string userEmail;
-        private readonly string userINN;
-        private readonly string queryText;
-        private readonly DateTime dateProviderEnd;
+        private readonly string _articleText;
+        private readonly string _articleTitle;
+        private readonly string _tagText;
+        private readonly string _configName;
+        private readonly string _providerName;
+        private readonly string _userLogin;
+        private readonly string _userEmail;
+        private readonly string _userInn;
+        private readonly string _queryText;
+        private readonly DateTime _dateProviderEnd;
+        private readonly DbContextOptions<EFContext> _contextOptions;
 
         public EFModelTest()
         {
-            userLogin = "123321";
-            userEmail = "superStrong@mail.com";
-            userINN = "123456789";
-            articleText = "Blah blah balh blah la la la la";
-            articleTitle = "1233";
-            tagText = "Blah";
-            configName = "Ypp";
-            providerName = "OOO MMM";
-            dateProviderEnd = DateTime.Now.AddDays(10);
-            queryText = "Мне сложно, помогите";
+            _contextOptions = new DbContextOptionsBuilder<EFContext>().UseInMemoryDatabase(databaseName: "Test_BD").Options;
+            _userLogin = "123321";
+            _userEmail = "superStrong@mail.com";
+            _userInn = "123456789";
+            _articleText = "Blah blah balh blah la la la la";
+            _articleTitle = "1233";
+            _tagText = "Blah";
+            _configName = "Ypp";
+            _providerName = "OOO MMM";
+            _dateProviderEnd = DateTime.Now.AddDays(10);
+            _queryText = "Мне сложно, помогите";
         }
 
         public void Dispose()
@@ -36,29 +39,37 @@ namespace WebApi.Test
         }
 
         [Fact]
+        public void Can_Initial_Context()
+        {
+            var context = new EFContext(_contextOptions);
+
+            Assert.NotNull(context);
+        }
+
+        [Fact]
         public void Can_Initial_Article()
         {
-            var article = new Article(articleTitle, articleText);
+            var article = new Article(_articleTitle, _articleText);
 
             Assert.NotNull(article);
-            Assert.Equal(articleTitle, article.Title);
-            Assert.Equal(articleText, article.Source);
+            Assert.Equal(_articleTitle, article.Title);
+            Assert.Equal(_articleText, article.Text);
         }
 
         [Fact]
         public void Can_Initial_Tag()
         {
-            var tag = new Tag(tagText);
+            var tag = new Tag(_tagText);
 
             Assert.NotNull(tag);
-            Assert.Equal(tagText, tag.Value);
+            Assert.Equal(_tagText, tag.Value);
         }
 
         [Fact]
         public void Can_Initial_ArticleTag()
         {
-            var tag = new Tag(tagText);
-            var article = new Article(articleTitle, articleText);
+            var tag = new Tag(_tagText);
+            var article = new Article(_articleTitle, _articleText);
 
             var articleTag = new ArticleTag(article, tag);
 
@@ -70,16 +81,16 @@ namespace WebApi.Test
         [Fact]
         public void Can_Initial_Configuration1c()
         {
-            var config = new Configuration1C(configName);
+            var config = new Configuration1C(_configName);
 
             Assert.NotNull(config);
-            Assert.Equal(configName, config.Name);
+            Assert.Equal(_configName, config.Name);
         }
 
         [Fact]
         public void Can_Initial_DependesArticle()
         {
-            var config = new Configuration1C(configName);
+            var config = new Configuration1C(_configName);
 
             var depenedes = new ArticleDependencies(config);
 
@@ -90,32 +101,32 @@ namespace WebApi.Test
         [Fact]
         public void Can_Initial_Provider()
         {
-            var provider = new Provider(providerName, dateProviderEnd);
+            var provider = new Provider(_providerName, _dateProviderEnd);
 
             Assert.NotNull(provider);
-            Assert.Equal(providerName, provider.Name);
-            Assert.Equal(dateProviderEnd, provider.ContractEndTime);
+            Assert.Equal(_providerName, provider.Name);
+            Assert.Equal(_dateProviderEnd, provider.ContractEndTime);
         }
 
         [Fact]
         public void Can_Initial_User()
         {
-            var provider = new Provider(providerName, dateProviderEnd);
+            var provider = new Provider(_providerName, _dateProviderEnd);
 
-            var user = new User(userLogin, userEmail, userINN, provider);
+            var user = new User(_userLogin, _userEmail, _userInn, provider);
 
             Assert.NotNull(user);
-            Assert.Equal(userEmail, user.Email);
-            Assert.Equal(userINN, user.INN);
-            Assert.Equal(userLogin, user.Login);
+            Assert.Equal(_userEmail, user.Email);
+            Assert.Equal(_userInn, user.INN);
+            Assert.Equal(_userLogin, user.Login);
             Assert.Equal(user.Provider, provider);
         }
 
         [Fact]
         public void Can_Initial_Session()
         {
-            var provider = new Provider(providerName, dateProviderEnd);
-            var user = new User(userLogin, userEmail, userINN, provider);
+            var provider = new Provider(_providerName, _dateProviderEnd);
+            var user = new User(_userLogin, _userEmail, _userInn, provider);
 
             var session = new Session(user);
 
@@ -126,25 +137,25 @@ namespace WebApi.Test
         [Fact]
         public void Can_Initial_Query()
         {
-            var provider = new Provider(providerName, dateProviderEnd);
-            var user = new User(userLogin, userEmail, userINN, provider);
+            var provider = new Provider(_providerName, _dateProviderEnd);
+            var user = new User(_userLogin, _userEmail, _userInn, provider);
             var session = new Session(user);
 
-            var query = new SearchingQuery(queryText, session);
+            var query = new SearchingQuery(_queryText, session);
 
             Assert.NotNull(query);
-            Assert.Equal(query.Text, queryText);
+            Assert.Equal(query.Text, _queryText);
             Assert.Equal(query.Session, session);
         }
 
         [Fact]
         public void Can_Initial_OpenedArticle()
         {
-            var provider = new Provider(providerName, dateProviderEnd);
-            var user = new User(userLogin, userEmail, userINN, provider);
+            var provider = new Provider(_providerName, _dateProviderEnd);
+            var user = new User(_userLogin, _userEmail, _userInn, provider);
             var session = new Session(user);
-            var query = new SearchingQuery(queryText, session);
-            var article = new Article(articleTitle, articleText);
+            var query = new SearchingQuery(_queryText, session);
+            var article = new Article(_articleTitle, _articleText);
             var date = DateTime.Now;
 
             var openedArticle = new OpenedArticle(date, article, query);
---
+++ Test/WebApi.Test/TagirationArticleTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using WebApi.EF.Models;
+using WebApi.Tagirator;
+using Xunit;
+
+namespace WebApi.Test
+{
+    public class TagirationArticleTest : IDisposable
+    {
+        [Fact]
+        public void Can_Initial()
+        {
+            var article = new Article(title, text);
+
+            TagirationArticle Article = new TagirationArticle(article);
+
+            Assert.NotNull(Article);
+
+            Assert.NotNull(Article.Article);
+
+            Assert.Equal(Article.Article.Title, title);
+            Assert.Equal(Article.Article.Text, text);
+        }
+
+        [Fact]
+        public void Not_Empty_Words()
+        {
+            var article = new Article(title, text);
+
+            var tagirationArticle = new TagirationArticle(article);
+
+            Assert.NotEmpty(tagirationArticle.CleanWords);
+        }
+
+        [Fact]
+        public void Can_Set_Current_Rate()
+        {
+            Article article = new Article(title, text);
+
+            var tagirationArticle = new TagirationArticle(article);
+            tagirationArticle.SetWordRate("я", 1);
+
+            var tag = tagirationArticle.GetTagsInArticle();
+            var key = tag.Keys.FirstOrDefault();
+
+            Assert.NotNull(key);
+            Assert.Equal(1, tag[key].Rate);
+        }
+
+        [Fact]
+        public void Can_Set_Current_Rate_With_More_Text()
+        {
+            Article article = new Article(title, text + "я я жопа да ло ло ло ло ло");
+
+            var tagirationArticle = new TagirationArticle(article);
+
+            tagirationArticle.SetWordRate("ло", 1);
+
+            var tag = tagirationArticle.GetTagsInArticle();
+            var key = tag.Keys.FirstOrDefault();
+
+            Assert.NotNull(key);
+            Assert.Equal(4, tag[key].Rate);
+        }
+
+        [Fact]
+        public void Can_Get_Tags()
+        {
+            Article Article = new Article(title, text);
+
+            var test = new TagirationArticle(Article).GetTagsInArticle();
+
+            Assert.NotNull(test);
+        }
+
+        [Fact]
+        public void Can_Get_Repeat_Freq()
+        {
+            Article Article = new Article(title, text);
+
+            var test = new TagirationArticle(Article).GetWordFrequancy("я");
+
+            Assert.Equal(2, test);
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private readonly string text;
+        private readonly string title;
+        private readonly string NullText;
+        private readonly string k;
+
+        public TagirationArticleTest()
+        {
+            title = "dad";
+            text = "Жопа я я";
+            NullText = "";
+            k = "я";
+        }
+    }
+}
