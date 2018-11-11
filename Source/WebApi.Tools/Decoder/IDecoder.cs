5af86fe5a887e12c560c8dabfe12eb5b8a914925
AnnRatn <annratnikova@mail.ru>
2018-11-11 19:22:47 +0300

    Добавлен декодер

---
+++ Source/WebApi.Tools/Decoder/IDecoder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApi.Tools.Decoder
+{
+    interface IDecoder
+    {
+        InnLogin GetInnLoginFromString(string encodeString);
+    }
+}
